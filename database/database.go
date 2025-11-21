7960f14a2bc29554268215cf8276133a10ac3332
Yohore Emmanuel <emmanuel.yohore@ynov.com>
2025-11-22 00:27:40 +0100

    trim space

--- database/database.go
+++ database/database.go
@@ -21,15 +21,10 @@ func Connect() {
 	}
 
 	host := os.Getenv("DB_HOST")
-
 	user := os.Getenv("DB_USER")
-
 	password := os.Getenv("DB_PASSWORD")
-
 	dbname := os.Getenv("DB_NAME")
-
 	port := os.Getenv("DB_PORT")
-
 	sslmode := os.Getenv("DB_SSLMODE")
 
 	dsn := fmt.Sprintf(
